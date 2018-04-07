8e620e0d4f4fd04952c7ec24bca7a3318c6af94a
Evans Chen <evansschen@gmail.com>
2018-04-07 10:14:24 -0500

    Committing initial project
    Added basic physics

---
+++ Klepticy/Assets/Scripts/MouseInputBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInputBehaviour : MonoBehaviour
+{
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // create a square on click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            Instantiate(prefab, mousePos, Quaternion.identity);
+        }
+        // track the highest
+        float time = Time.realtimeSinceStartup;
+        float maxYPos = float.MinValue;
+        foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Trash"))
+        {
+            Bounds bounds = gameObject.GetComponent<BoxCollider2D>().bounds;
+            float yPos = (bounds.center - bounds.extents).y;
+            if (maxYPos < yPos)
+            {
+                maxYPos = yPos;
+            }
+        }
+        float total = Time.realtimeSinceStartup - time;
+        Debug.Log(maxYPos + " " + total);
+    }
+}

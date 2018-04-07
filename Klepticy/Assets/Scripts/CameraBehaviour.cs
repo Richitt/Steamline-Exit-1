a4ade7a985a90f4842e4fcaf06f43e94522c6214
Evans Chen <evansschen@gmail.com>
2018-04-07 12:48:06 -0500

    Added more walls when you build high
    More walls is more sad

--- Klepticy/Assets/Scripts/CameraBehaviour.cs
+++ Klepticy/Assets/Scripts/CameraBehaviour.cs
@@ -6,15 +6,56 @@ public class CameraBehaviour : MonoBehaviour {
 
     public GameObject follow;
     float ySpeed = 0;
+    public List<GameObject> walls = new List<GameObject>();
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
+    {
+        MoveCameraSmooth();
+        UpdateWalls();
+    }
+
+    // if the camera bounds exceed the walls
+    private void UpdateWalls()
+    {
+        // get camera bounds
+        Bounds cameraBounds = OrthographicBounds();
+        float cameraTop = (cameraBounds.center + cameraBounds.extents).y;
+        // wall bounds to compare to camera bounds
+        for (int i = 0; i < walls.Count; i++)
+        {
+            GameObject wall = walls[i];
+            Bounds wallBounds = wall.GetComponent<BoxCollider2D>().bounds;
+            float wallTop = (wallBounds.center + wallBounds.extents).y;
+            // add more walls if necessary
+            if (cameraTop > wallTop)
+            {
+                walls[i] = Instantiate(wall,
+                    new Vector3(wall.transform.position.x,
+                    wall.transform.position.y + (wallBounds.extents.y * 2),
+                    wall.transform.position.z), Quaternion.identity);
+            }
+        }
+    }
+
+    // gets camera bounds
+    private Bounds OrthographicBounds()
+    {
+        float screenAspect = Screen.width / Screen.height;
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Bounds bounds = new Bounds(
+            Camera.main.transform.position,
+            new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        return bounds;
+    }
+
+    private void MoveCameraSmooth()
     {
         // follow the specified object
         // to smooth the following, use a differential equation
@@ -26,10 +67,10 @@ public class CameraBehaviour : MonoBehaviour {
         float targetSpd = k1 * (targetPos - currentPos);
         float k2 = 0.1f;
         float acc = k2 * (targetSpd - ySpeed);
+        // move the y position of the camera to the calculated value
         ySpeed += acc;
         currentPos += ySpeed;
-        
         transform.position = new Vector3(transform.position.x,
             currentPos, transform.position.z);
-	}
+    }
 }
